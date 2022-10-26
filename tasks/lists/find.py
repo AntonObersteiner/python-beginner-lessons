87bb25a95c7e6fa840900abd159f4d1b39859e3b
AntonObersteiner <anton.obersteiner@gmail.com>
2022-10-27 01:43:04 +0200

    billige Kopie von marks: zweite Listen-Aufgabe

---
+++ tasks/lists/find.py
@@ -0,0 +1,42 @@
+# In der Liste values stehen Namen, manchmal auch mehrmals
+# In jeder der folgenden Aufgaben wird die Variable `name` das Verhalten steuern.
+# testet die richtigkeit eures Codes für verscheidene Werte von Name
+# (je nach Aufgabe, eventuell auch Namen, die gar nicht vorkommen)
+name = "Milo"
+#
+# 1a) zählt, wie oft der Name in der Liste vorkommt! ("Milo": 2 mal)
+# 1b) zählt für alle Namen, wie oft sie vorkommen (extra: gebt das nur einmal pro namen aus)
+# 1c) welche Name kommt am häufigsten vor? ("Michael" und ein anderer...)
+#
+# 2a) wo kommt der Name zuerst vor? gebt den Index zurück ("Milo": 1)
+# 2b) wenn ein Name mehrfach vorkommt: wo kommt der Name zum n-ten Mal vor? ("Milo" kommt zum 2-ten Mal bei 65 vor)
+# was macht euer Code, wenn ihr n zu hoch setzt?
+n = 2
+#
+#
+# 3a) Wie viele Namen in der Liste fangen mit demselben Buchstaben an wie der Name ("Milo" -> "M": 17)
+# 3b) Wie viele Namen fangen mit den gleichen zwei oder drei (z.B. Variable `n` verwenden) an? ("Mi": 12, "Mil": 6)
+#
+# Hinweise:
+#   - liste.append(x) fügt x an liste an
+#   - len(liste) gibt die Länge einer Liste zurück
+#   - liste[0] gibt das Element an Stelle 0 zurück
+#   - liste[1] = 3.0 setzt das Element an Stelle 1 auf 3.0
+#   - auf strings könnt ihr zugreifen, wie auf Listen: "Hallo"[1] -> "a"
+#   - einen Abschnitt einer Liste könnt ihr mit Liste[von:bis] bekommen:
+#       Liste = [0, 1, 2, 3]
+#       Liste[1:3] -> [1, 2]
+
+
+Liste = ["Sophia", "Milo", "Juna", "Noah", "Felix", "Julia", "Mila", "Michael", "Lea", "Anna", "Nele", "Luis", "Nala", "Moritz", "Anna", "Levin", "Sebastian", "Charlotte", "Laura", "Julia", "Julian", "Emma", "Tanja", "Lina", "Emil", "Levin", "Lina", "Nala", "Anton", "Ben", "Luca", "Aaron", "Nina", "Laura", "Emil", "Mila", "Nala", "Emily", "Mila", "Luca", "Lukas", "Lia", "Luisa", "Finn", "Sophia", "Michael", "Sophia", "Matteo", "Julian", "Lea", "Sebastian", "Ava", "Amelie", "Moritz", "Charlotte", "Darian", "Mila", "Lea", "Julian", "Johanna", "Linus", "Michael", "Felix", "Sophie", "Milo", "Mira", "Alexander", "Marcel", "Julia", "Noah", "Luca", "Simon", "Jana", "Alexander", "Nelio", "Finn", "Sarah", "Michael", "Laura", "Thomas", "Valentin", "Lea", "Lina", "Laura", "Nelio", "Leonie", "Michael", "Paula", "Sophie", "Anton", "Juna", "Emil", "Jonathan", "Elena", "Juna", "Finn", "Charlotte", "Moritz"]
+
+# 1.)
+
+
+
+# 2.)
+
+# printe anzahl der Studierenden
+
+
+# printe den Durchschnitt
